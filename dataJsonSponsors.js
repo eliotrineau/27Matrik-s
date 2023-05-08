d4ebf57704d00c10e0e2326209dbe6f20aad6285
eliotrineau <eliot.rineau@edu.ece.fr>
2023-05-08 13:47:14 +0200

    nouvelle police (la deuxième)

--- dataJsonSponsors.js
+++ dataJsonSponsors.js
@@ -9,7 +9,7 @@ const getDataSponsors = async() => {
         sponsorsContainer.innerHTML +=
         `
         <div class="border border-[#008F11] rounded-xl group">
-            <h2 class="text-2xl lg:text-5xl text-center text-[#008F11] mt-2 group-hover:text-[#00FF41]">${sponsors.nom} nous soutient !</h2>
+            <h2 class="text-2xl lg:text-5xl text-center text-[#008F11] mt-2 group-hover:text-[#00FF41]" style="font-family: 'Rum Raisin', cursive;">${sponsors.nom} nous soutient !</h2>
             <div class="flex-col">
                 <a class="flex justify-center" target="_blank" href="${sponsors.lien}"><img class="my-4 w-24 h-24 lg:w-52 lg:h-52 group-hover:scale-125 transition duration-150 ease-in-out" srcset="${sponsors.img} 1x, ${sponsors.img} 2x" src="${sponsors.img}"></a>
                 <p class="text-sm lg:text-xl mb-4 text-center text-[#008F11] m-2">${sponsors.texte}</p>
