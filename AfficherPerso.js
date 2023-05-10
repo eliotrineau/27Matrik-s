58b34b7760aadc04faaa0261ada067f5e54e6160
eliotrineau <eliot.rineau@edu.ece.fr>
2023-05-10 18:41:18 +0200

    push mais la fonciton concours n'est pas encore au point

--- AfficherPerso.js
+++ AfficherPerso.js
@@ -1,6 +1,9 @@
+//import de firebase 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
 import { getFirestore, collection, addDoc, updateDoc, deleteDoc, setDoc, getDoc, where, writeBatch, query, orderBy, doc, limit, getDocs } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
 
+
+//mon api BDD
 const firebaseConfig = {
   apiKey: "AIzaSyCJHQlxEfS_1rbnX-5OLA8B7QGEz28FKRM",
   authDomain: "fir-27matrik-s.firebaseapp.com",
@@ -10,12 +13,12 @@ const firebaseConfig = {
   appId: "1:407433059580:web:0dc8ff8164dcb657fbcb79"
 };
 
-
+//initialiser firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 
-
+//récuperer un document sur firebase
 const getDocument = async (collectionName) => {
   const DocumentColRef = collection(db, collectionName);
   const DocumentSnapshot = await getDocs(DocumentColRef);
@@ -24,49 +27,29 @@ const getDocument = async (collectionName) => {
 }
 
 
-
-const userExist = async (name, password) => {
-
-  const DocumentColRef = collection(db, "users");
-  const q = await query(DocumentColRef, where("name", "==", name), where("password", "==", password))
+//vérifier qu'un utilisateur de la collection Utilisateurs existe
+const userExist = async (pseudo, motdepasse) => {
+  const DocumentColRef = collection(db, "Utilisateurs");
+  const q = query(DocumentColRef, where("pseudo", "==", pseudo), where("motdepasse", "==", motdepasse))
   const querySnapshot = await getDocs(q);
-  console.log("querySnapshot docs", querySnapshot.docs)
   const DocumentList = querySnapshot.docs.map(doc => ({...doc.data(), id: doc.id}));
-  console.log('test user already exists', name, password, DocumentList);
   return DocumentList;
 };
 
-// userExist("enzo", "1234")
-// userExist("enzo", "test")
-// fonction pour créer une collection (CREATE)
+
+// fonction pour créer une collection
 const createDocument = async (collectionName, newObj) => {
-  console.log('createDocument', newObj)
   const DocumentColRef = collection(db, collectionName);
   const DocumentSnapshot = await addDoc(DocumentColRef, newObj);
 }
-// createDocument("burgers", {name: "cheeseburger"})
-
-// fonction pour mettre à jour une collection (UPDATE)
-const updateDocument = async (collectionName, newObj) => {
-  console.log('updateDocument', newObj)
-  const DocumentColRef = doc(db, collectionName, newObj.id)
-  const DocumentSnapshot = await updateDoc(DocumentColRef, newObj);
-}
-// fonction pour supprimer une collection (DELETE)
-const deleteDocument = async (collectionName, id) => {
-  console.log('deleteDocument', id)
-  const DocumentColRef = doc(db, collectionName, id)
-  console.log('DocumentColRef', DocumentColRef)
-  await deleteDoc(DocumentColRef, id);
-}
 
 
+//fonction pour afficher tous les utilisateurs qui existe
 const afficherUtilisateurs = async () => {
   const utilisateurs = await getDocument("Utilisateurs");
-  console.log(utilisateurs);
   const utilisateursHTML = document.querySelector("#utilisateursHTML");
   utilisateurs.forEach((utilisateurs)=>{
-    utilisateursHTML.innerHTML +=
+    utilisateursHTML.innerHTML += //affiche le html
     `
     <div class="flex-col border border-[#008F11]] text-[#008F11] text-xl">
         <p>${utilisateurs.pseudo}</p>
@@ -76,44 +59,85 @@ const afficherUtilisateurs = async () => {
     `;
   })
 }
-afficherUtilisateurs();
+// ne pas appeler car nous voyons toutes les données des gens afficherUtilisateurs();
 
-
+//fonction pour inscrire un utilisateur
 const Inscription = async (pseudo, email, motdepasse) => {
+  const texteHTMLIns = document.querySelector("#texteHTMLIns"); // on lie le code à l'html avec l'id
   try {
-    const utilisateurExists = await userExist(pseudo, motdepasse);
+    const utilisateurExists = await userExist(pseudo, motdepasse); //vérifie si un utilisateur possédant le même pseudo et mot de passe existe déjà
     if (utilisateurExists.length > 0) {
+      texteHTMLIns.innerHTML ="" //vide le html
+      texteHTMLIns.innerHTML += //affiche le hmtl
+      `
+      <div class="justify-center">
+        <p class="text-[#008F11] text-xl">L'utilisateur existe déjà !</p>
+      </div>
+      `;
       alert("L'utilisateur existe déjà");
       return;
     }
-    const nouvelUtilisateur = {
+    const nouvelUtilisateur = { //creer obj utilisateur
       pseudo,
       email,
       motdepasse,
     };
+    texteHTMLIns.innerHTML ="" //vide le html
+    texteHTMLIns.innerHTML += //affiche le hmtl
+    `
+    <div class="justify-center">
+      <p class="text-[#008F11] text-xl">Inscription réussie !</p>
+    </div>
+    `;
     await createDocument("Utilisateurs", nouvelUtilisateur); //ajouter l'utilisateur à Firebase
   } catch (error) {
-    console.error("Erreur lors de l'inscription de l'utilisateur", error);
-    alert("Une erreur est survenue lors de l'inscription de l'utilisateur");
+      texteHTMLIns.innerHTML ="" //vide le html
+      texteHTMLIns.innerHTML += //affiche le hmtl
+      `
+      <div class="justify-center">
+        <p class="text-[#008F11] text-xl">Une erreur est survenue lors de l'inscription de l'utilisateur !</p>
+      </div>
+      `;
   }
 };
 
 
+
 const Connexion = async (pseudo, motdepasse) => {
+  const texteHTMLCon = document.querySelector("#texteHTMLCon"); // on lie le code à l'html avec l'id
   try {
     const q = query(collection(db, "Utilisateurs"), where("pseudo", "==", pseudo)); // récupère l'utilisateur avec le pseudo correspondant
     const querySnapshot = await getDocs(q);
 
-    if (querySnapshot.empty) {
-      alert("L'utilisateur n'existe pas"); // affiche un message d'erreur si l'utilisateur n'existe pas
+    if (querySnapshot.empty) { //si le document recherché n'existe pas / vide alors :
+      texteHTMLCon.innerHTML=""  //vide le html
+      texteHTMLCon.innerHTML += //affiche le hmtl
+      `
+      <div class="justify-center">
+        <p class="text-[#008F11] text-xl">L'utilisateur n'existe pas !</p>
+      </div>
+      `;
     } else {
       const utilisateur = querySnapshot.docs[0].data(); // récupère les données de l'utilisateur
       if (utilisateur.motdepasse !== motdepasse) { // vérifie si le mot de passe est incorrect
-        alert("Mot de passe incorrect");
+        texteHTMLCon.innerHTML="" //affiche le hmtl
+        texteHTMLCon.innerHTML += //affiche le hmtl
+        `
+        <div class="justify-center">
+          <p class="text-[#008F11] text-xl">mot de passe incorrect !</p>
+        </div>
+        `;
       } else {
-        alert("Connexion réussie !");
+        texteHTMLCon.innerHTML="" //affiche le hmtl
+        texteHTMLCon.innerHTML += //affiche le hmtl
+        `
+        <div class="justify-center">
+          <p class="text-[#008F11] text-xl">Connexion réussie !</p>
+        </div>
+        `;
         const afficherUtilisateur = document.querySelector("#utilisateur-container"); // affiche les données de l'utilisateur
-        afficherUtilisateur.innerHTML += 
+        afficherUtilisateur.innerHTML =""  //affiche le hmtl
+        afficherUtilisateur.innerHTML +=  //affiche le hmtl
         `
         <div class="flex-col border border-[#008F11]] text-[#008F11] text-xl">
           <p>${utilisateur.pseudo}</p>
@@ -123,37 +147,96 @@ const Connexion = async (pseudo, motdepasse) => {
       }
     }
   } catch (error) {
-    console.error("Erreur lors de la connexion de l'utilisateur", error);
-    alert("Une erreur est survenue lors de la connexion de l'utilisateur");
+      texteHTMLCon.innerHTML="" //affiche le hmtl
+      texteHTMLCon.innerHTML += //affiche le hmtl
+      `
+      <div class="justify-center">
+        <p class="text-[#008F11] text-xl">Une erreur est survenue lors de la connexion de l'utilisateur !</p>
+      </div>
+      `;
   }
 };
 
 const supprimerUtilisateur = async (pseudo, motdepasse) => {
+  const texteHTMLSup = document.querySelector("#texteHTMLSup"); // on lie le code à l'html avec l'id
   try {
     // Vérifie si l'utilisateur existe et récupère ses données
     const q = query(collection(db, "Utilisateurs"), where("pseudo", "==", pseudo));
     const querySnapshot = await getDocs(q);
-    if (querySnapshot.empty) {
-      alert("L'utilisateur n'existe pas");
+    if (querySnapshot.empty) { //si le document recherché n'existe pas / vide alors :
+      texteHTMLSup.innerHTML="" //affiche le hmtl
+      texteHTMLSup.innerHTML += //affiche le hmtl
+      `
+      <div class="justify-center">
+        <p class="text-[#008F11] text-xl">L'utilisateur n'existe pas !</p>
+      </div>
+      `;
       return;
     }
     const utilisateur = querySnapshot.docs[0].data();
 
     // Vérifie si le mot de passe est correct
     if (utilisateur.motdepasse !== motdepasse) {
-      alert("Mot de passe incorrect");
+      texteHTMLSup.innerHTML="" //affiche le hmtl
+      texteHTMLSup.innerHTML += //affiche le hmtl
+      `
+      <div class="justify-center">
+        <p class="text-[#008F11] text-xl">Mot de passe incorrect !</p>
+      </div>
+      `;
       return;
     }
 
     // Supprime l'utilisateur
-    const utilisateurRef = doc(db, "Utilisateurs", querySnapshot.docs[0].id);
-    await deleteDoc(utilisateurRef);
-    alert("L'utilisateur a été supprimé avec succès !");
+    const utilisateurRef = doc(db, "Utilisateurs", querySnapshot.docs[0].id); //cherche l'id de l'utilisateur
+    await deleteDoc(utilisateurRef); //supprime le document correspondant de la BDD firebase
+    texteHTMLSup.innerHTML="" //affiche le hmtl
+    texteHTMLSup.innerHTML += //affiche le hmtl
+    `
+    <div class="justify-center">
+      <p class="text-[#008F11] text-xl">L'utilisateur a été supprimé avec succès !</p>
+    </div>
+    `;
   } catch (error) {
-    console.error("Erreur lors de la suppression de l'utilisateur", error);
-    alert("Une erreur est survenue lors de la suppression de l'utilisateur");
+    texteHTMLSup.innerHTML="" //affiche le hmtl
+    texteHTMLSup.innerHTML += //affiche le hmtl
+    `
+    <div class="justify-center">
+      <p class="text-[#008F11] text-xl">Une erreur est survenue lors de la suppression de l'utilisateur !</p>
+    </div>
+    `;
   }
 };
+
+/*
+const faireGagner = async () => {
+  const gagnantHTML = document.querySelector("#gagnantHTML"); // lie le code à l'html avec l'id
+  const Collection = collection(db, "Utilisateurs"); // récupère la collection Utilisateurs
+  try {
+    const snapshot = await getDocs(Collection);
+    const nbDoc = snapshot.size; // nombre de documents
+    const nbAleatoire = Math.floor(Math.random() * nbDoc); // génère un nombre aléatoire
+    const querySnapshot = await Collection.limit(1).offset(nbAleatoire).get(); // spécifie l'offset et limite à un seul résultat
+    querySnapshot.forEach((doc) => { // affiche l'html
+      const gagnant = doc.data().pseudo;
+      gagnantHTML.innerHTML =""
+      gagnantHTML.innerHTML += `
+        <div class="justify-center">
+          <p class="text-[#008F11] text-xl">Le gagnant est : ${gagnant}!</p>
+        </div>
+      `;
+    });
+  } catch (error) {
+    gagnantHTML.innerHTML =""
+    gagnantHTML.innerHTML += `
+      <div class="justify-center">
+        <p class="text-[#008F11] text-xl">Un problème est survenu !</p>
+      </div>
+    `;
+  }
+};
+*/
+
 
 const EcouterInscription = async (event) => {
   event.preventDefault(); // permet d'éviter de recharger la page en continu
@@ -184,4 +267,5 @@ const btnConnexion = document.querySelector("#btnConnexion")
 btnConnexion.addEventListener("click", EcouterConnexion);
 const btnSupprimer = document.querySelector("#btnSupprimer");
 btnSupprimer.addEventListener("click", EcouterSupprimer);
-
+const btnFaireGagner = document.querySelector("#btnFaireGagner");
+btnFaireGagner.addEventListener("click", faireGagner);
